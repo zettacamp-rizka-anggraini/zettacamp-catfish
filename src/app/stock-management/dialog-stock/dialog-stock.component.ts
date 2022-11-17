a56e3178f6f8c376681b2f8b00c5cc8c30439d64
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-11-17 17:07:38 +0700

    [Updated] Sweet Alert

--- src/app/stock-management/dialog-stock/dialog-stock.component.ts
+++ src/app/stock-management/dialog-stock/dialog-stock.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SubSink } from 'subsink';
 import { StockManagementService } from '../stock-management.service';
 import Swal from 'sweetalert2'
@@ -17,7 +17,7 @@ export class DialogStockComponent implements OnInit, OnDestroy {
   formStock: FormGroup;
   addData:boolean = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any,private serviceStock:StockManagementService, private fb:FormBuilder) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any,private serviceStock:StockManagementService, private fb:FormBuilder, private dialogRef: MatDialogRef<DialogStockComponent>) { }
 
   ngOnInit(): void {
     
@@ -27,7 +27,7 @@ export class DialogStockComponent implements OnInit, OnDestroy {
   initForm(){
     this.formStock = this.fb.group({
       name: ['', [Validators.required]],
-      stock: ['', [Validators.required]]
+      stock: ['', [Validators.required, Validators.min(1)]]
     })
 
     if(this.data){
@@ -44,35 +44,73 @@ export class DialogStockComponent implements OnInit, OnDestroy {
 
   //kurang swal untuk error server and server
   onSubmit(){
+    const updatedId = this.id;
+    const updatedValue = this.formStock.value;
     if(this.id){
-      const updatedId = this.id;
-      const updatedValue = this.formStock.value;
       if(this.formStock.valid){
-        this.subs.sink = this.serviceStock.updateStock(updatedId, updatedValue.name, updatedValue.stock).subscribe(()=>{
-          this.serviceStock.getAllStock().refetch();
+        this.subs.sink = this.serviceStock.updateStock(updatedId, updatedValue.name, updatedValue.stock).subscribe({
+          next:()=>{
+            Swal.fire({
+              title: "Updated",
+              text: "Data Has Been Updated",
+              icon: "success",
+              confirmButtonText: "Ok"
+            }).then(()=>{
+              this.serviceStock.getAllStock().refetch();
+              this.dialogRef.close();
+            });
+          },
+          error:()=>{
+            Swal.fire({
+              title: "Error!",
+              text: "Something Happend!",
+              icon: "error",
+              confirmButtonText: "OK"
+            });
+          }
         });
+      }else{
         Swal.fire({
-          title: 'Success!',
-          text: 'Data Stock Has Been Updated',
-          icon: 'success',
-          confirmButtonText: 'Close'
+          title: "Error!",
+          text: "Data Invalid!",
+          icon: "error",
+          confirmButtonText: "OK"
         });
-      }
+      };
     } else {
       const stockData = this.formStock.value;
       if(this.formStock.valid){
-        this.subs.sink = this.serviceStock.createNewStock(stockData.name, stockData.stock).subscribe(()=>{
-          this.serviceStock.getAllStock().refetch();
+        this.subs.sink = this.serviceStock.createNewStock(stockData.name, stockData.stock).subscribe({
+          next:()=>{
+            Swal.fire({
+              title: "Success",
+              text: "Data Has Been Added",
+              icon: "success",
+              confirmButtonText: "Ok"
+            }).then(()=>{
+              this.dialogRef.close();
+              this.serviceStock.getAllStock().refetch();
+            });
+          },
+          error:()=>{
+            Swal.fire({
+              title: "Error!",
+              text: "Something Happend!",
+              icon: "error",
+              confirmButtonText: "OK"
+            });
+          }
         });
+      } else {
         Swal.fire({
-          title: 'Success!',
-          text: 'Data Stock Has Been Created',
-          icon: 'success',
-          confirmButtonText: 'Close'
+          title: "Error!",
+          text: "Data Invalid!",
+          icon: "error",
+          confirmButtonText: "OK"
         });
-      }
-    }
-  }
+      };
+    };
+  };
 
   ngOnDestroy(): void {
     this.subs.unsubscribe();
