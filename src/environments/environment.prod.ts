3073d0824c9f8243fd57ade2cd66e10b9fb665ce
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-11-26 14:09:41 +0700

    [updated] environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: 'https://5bf5-114-142-171-57.ap.ngrok.io',
+  tokenKey: 'token',
+  role: 'role',
+  usertype: 'usertype'
 };
