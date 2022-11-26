11aa51d7e0179783d238e60385f1e3456320d21f
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-11-26 10:22:18 +0700

    [Add] Guard Routing

---
+++ src/app/guard/cart-page.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartPageGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const token = localStorage.getItem(environment.tokenKey);
+    const role = JSON.parse(localStorage.getItem(environment.role));
+    if (token !== null && ( role == 'admin' || role=='user') ) {
+      return true;
+    } else {
+      this.router.navigate(['main-page']);
+    }
+  }
+}
---
+++ src/app/guard/stock-management.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockManagementGuard implements CanActivate {
+  constructor(private router:Router){};
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = localStorage.getItem(environment.tokenKey);
+    const role = JSON.parse(localStorage.getItem(environment.role));
+    if(token !== null && role == 'admin'){
+      return true
+    }else{
+      this.router.navigate(['main-page']);
+    }
+  }
+  
+}
