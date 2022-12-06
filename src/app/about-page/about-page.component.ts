e431c611facf85cd7f27723f357c291d9ea21f59
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-12-06 20:36:19 +0700

    [update] localization

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about-page',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-page.component.css']
 })
 export class AboutPageComponent implements OnInit {
-
+  role:any = null;
   constructor() { }
 
   ngOnInit(): void {
+    this.role = JSON.parse(localStorage.getItem(environment.role));
   }
 
 }
--- src/app/about-page/about-page.module.ts
+++ src/app/about-page/about-page.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './about-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../app.module';
 
 const routes : Routes = [
   {path:"", component:AboutPageComponent}
@@ -15,7 +18,15 @@ const routes : Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MaterialModule
+    MaterialModule,
+      TranslateModule.forChild({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        },
+        defaultLanguage:'en', 
+      }),
   ],
   exports:[
     AboutPageComponent
