d1905104f0a333c09b466126ad956ed9f495be90
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-12-06 14:01:29 +0700

    [Add] forgot password

---
+++ src/app/login-page/password-dialog/password-dialog.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SubSink } from 'subsink';
+import Swal from 'sweetalert2';
+import { LoginPageService } from '../login-page.service';
+
+@Component({
+  selector: 'app-password-dialog',
+  templateUrl: './password-dialog.component.html',
+  styleUrls: ['./password-dialog.component.css']
+})
+export class PasswordDialogComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
+  hide:boolean = true;
+  resetForm: FormGroup;
+  constructor(private fb:FormBuilder, private serviceReset: LoginPageService, @Inject(MAT_DIALOG_DATA) private data:any, private dialogRef:MatDialogRef<PasswordDialogComponent>) { }
+
+  ngOnInit(): void {
+    this.initResetForm();
+  }
+
+  initResetForm(){
+    this.resetForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(8)]]
+    });
+    this.resetForm.patchValue(this.data);
+  }
+
+  resetPassword(){
+    const payload = this.resetForm.value;
+    if(this.resetForm.valid){
+      this.subs.sink = this.serviceReset.resetPassword(payload).subscribe({
+        next: ()=>{
+          Swal.fire(
+            'Success!',
+            'your password has been reset',
+            'success'
+          ).then(()=>{
+            this.dialogRef.close();
+          })
+        },
+        error: (error)=>{
+          Swal.fire(
+            'Error!',
+            error.message,
+            'error'
+          )
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+}
