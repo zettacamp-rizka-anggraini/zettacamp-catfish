b4fe8cc48a6854125e4e1347692258d490088f9a
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-12-13 16:54:25 +0700

    [fixing] bug

--- src/app/menu-management/detail-menu/detail-menu.component.ts
+++ src/app/menu-management/detail-menu/detail-menu.component.ts
@@ -23,7 +23,9 @@ export class DetailMenuComponent implements OnInit, OnDestroy {
   }
 
   onImageError(event){
-    event.target.src = "https://images.unsplash.com/photo-1534939561126-855b8675edd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    if(this.dataMenu != null){
+      event.target.src = "https://images.unsplash.com/photo-1534939561126-855b8675edd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+    }
   }
 
   ngOnDestroy(): void {
