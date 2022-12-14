f2dbf815f1361a045dfa4db50bc00f471ba0b845
zettacamp-rizka-anggraini <rizka.anggraini@zettacamp.pro>
2022-12-14 10:23:58 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://api.team-7.zettacamp-upn.zetta-demo.space/graphql',
+  apiUrl: 'https://api.team-7.zettacamp-upn.zetta-demo.space/graphql',
   // apiUrl: 'https://tired-petticoat-slug.cyclic.app',
   tokenKey: 'token',
   user_id: 'user_id',
